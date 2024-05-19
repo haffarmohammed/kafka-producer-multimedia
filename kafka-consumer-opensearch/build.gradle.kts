48a1a13de66b29a6e8b94ab1b20a155a3cf2c95e
Mohammed HAFFAR <mohammedhaffar@MacBook-Pro-de-Mohammed.local>
2024-05-19 22:38:19 +0200

    add : create index wikimedia

--- kafka-consumer-opensearch/build.gradle.kts
+++ kafka-consumer-opensearch/build.gradle.kts
@@ -10,8 +10,20 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+    implementation("org.apache.kafka:kafka-clients:3.3.1")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.13")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+
+    // https://central.sonatype.com/artifact/org.opensearch.client/opensearch-rest-high-level-client/overview
+    implementation("org.opensearch.client:opensearch-rest-high-level-client:2.14.0")
+
+    // https://central.sonatype.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.11.0")
 }
 
 tasks.test {
